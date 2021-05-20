9dd7f8dd7f6f6f9f17eb4fe6917d850f5d7cfece
dragonman164 <sanidhiyafirefox123@gmail.com>
2021-05-20 19:24:10 +0530

    CSE5 and Project Euler

---
+++ CSE5 Problems/Introductory Problems/NumberSpiral.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> 
+#define ll long long 
+using namespace std;
+void solve()
+{
+    ll t, y, x;
+    cin>>t;
+    while(t--)
+    {
+        cin>>y>>x;
+    }
+}
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+solve();
+return 0;
+}
